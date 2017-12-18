9dc46c2396e1034d3dac89f5fbede71e38d70adf
Aubron Wood <aub676@gmail.com>
2017-12-18 12:44:50 -0500

    React setup, router, etc

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
+import { Router, Route, Switch } from 'react-router'
 import logo from './logo.svg';
-import './App.css';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+
+import IconButton from 'material-ui/IconButton';
+import Github from './components/Github';
+const styles = {
+  root: {
+    width: '100%',
+  },
+  grow: {
+    flex: '1 1 auto',
+  },
+};
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={classes.root}>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <Typography type="title" color="inherit">
+              CloudArk 1.0
+            </Typography>
+            <div className={classes.grow} />
+            <IconButton
+              component="a"
+              title="GitHub"
+              color="contrast"
+              href="https://github.com/mui-org/material-ui"
+            >
+              <Github />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
